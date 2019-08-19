63ba557ee50c8f3d10e627602d1432c7d07e0080
Bryan Dina <bryansdina@gmail.com>
2019-08-19 16:24:24 -0400

    use scala concurrency (futures)

--- src/main/scala/app/Scant.scala
+++ src/main/scala/app/Scant.scala
@@ -1,7 +1,8 @@
 package app
 
-import protocol._
+import java.net.InetAddress
 
+import protocol._
 import java.util.Properties
 import java.util.logging.Logger
 
@@ -27,34 +28,54 @@ object Scant {
   def main(args: Array[String]): Unit = {
 
     import java.util.concurrent.TimeUnit
+    import scala.concurrent.Await
+    import scala.concurrent.ExecutionContext.Implicits.global
+    import scala.concurrent.Future
+    import scala.concurrent.duration._
+    import scala.language.postfixOps
+    import scala.util.{Failure, Success}
 
     val ipProvider   = IpProviderFactory.create()
     val dnsProvider  = new SimpleDNSProvider()
     val ddnsProvider = new NamecheapDDNSProvider()
 
+    def ip_lookup = Future { ipProvider.address() }
+    def dns_lookup (host: String, domain: String) = Future { dnsProvider.address(host, domain) }
+
+    val (host, domain) = hostAndDomain()
+
     val daemon = if ( args.length == 1 ) { "-d".equals(args(0)) } else { false }
 
     do {
-      ipProvider.address() match {
-        case None => logger.severe("unable to fetch external ip from router!")
-        case Some(externalIp) =>
-          val (host, domain) = hostAndDomain()
+      val externalIp = ip_lookup
+      val dnsIp      = dns_lookup(host, domain)
 
-          logger.info(s"configured to update host '$host' for domain '$domain'")
+      val result = for {
+        host_ip <- externalIp
+        dns_ip <- dnsIp
+      } yield (host_ip, dns_ip)
 
-          dnsProvider.address(host, domain) match {
-            case None => logger.severe("unable to fetch host record from dns!")
-            case Some(dnsIp) =>
-              val expected = externalIp
-              val actual = dnsIp
-
-              if (actual != expected) {
-                logger.info(s"updating DNS with $expected")
-                ddnsProvider.update(host,domain,externalIp)
-              }
-        }
+      if (!daemon) {
+        Await.result(externalIp, 10 second)
+        Await.result(dnsIp, 10 second)
+      } else {
+        TimeUnit.MINUTES.sleep(1L)
       }
-      if (daemon) { TimeUnit.MINUTES.sleep(1L) }
+
+      result onComplete {
+        case Success(value) =>
+          value match {
+            case (Some(externalIp: InetAddress), Some(dnsIp: InetAddress)) =>
+              if (!externalIp.equals(dnsIp)) {
+                logger.info(s"updating DNS with $externalIp")
+                ddnsProvider.update(host, domain, externalIp)
+              }
+            case (None, Some(_)) => logger.severe("unable to fetch external IP!")
+            case (Some(_), None) => logger.severe("unable to fetch host record")
+            case (None, None) => logger.severe("unable to fetch external IP and host record")
+          }
+        case Failure(exception) => logger.severe(s"unable to process: ${exception.getMessage}")
+      }
     } while (daemon)
   }
 }
