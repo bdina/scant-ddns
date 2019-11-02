4aa9cade27e355cc99a44c691303f1f47d84a1b1
Bryan Dina <bryansdina@gmail.com>
2019-11-02 07:22:16 -0400

    extend App trait

--- src/main/scala/app/Scant.scala
+++ src/main/scala/app/Scant.scala
@@ -5,7 +5,7 @@ import java.net.InetAddress
 import protocol._
 import java.util.Properties
 
-object Scant extends ScantLogging {
+object Scant extends App with ScantLogging {
 
   def configuration(): Properties = {
     import java.nio.file.{Files,Paths,StandardOpenOption}
@@ -22,57 +22,54 @@ object Scant extends ScantLogging {
     (Host(host), Domain(domain))
   }
 
-  def main(args: Array[String]): Unit = {
+  import java.util.concurrent.TimeUnit
+  import scala.concurrent.Await
+  import scala.concurrent.ExecutionContext.Implicits.global
+  import scala.concurrent.Future
+  import scala.concurrent.duration._
+  import scala.language.postfixOps
+  import scala.util.{Failure, Success}
 
-    import java.util.concurrent.TimeUnit
-    import scala.concurrent.Await
-    import scala.concurrent.ExecutionContext.Implicits.global
-    import scala.concurrent.Future
-    import scala.concurrent.duration._
-    import scala.language.postfixOps
-    import scala.util.{Failure, Success}
+  val ipProvider   = ExternalIPProvider()
+  val dnsProvider  = SimpleDNSProvider()
+  val ddnsProvider = NamecheapDDNSProvider()
 
-    val ipProvider   = ExternalIPProvider()
-    val dnsProvider  = SimpleDNSProvider()
-    val ddnsProvider = NamecheapDDNSProvider()
+  def ip_lookup = Future { ipProvider.address() }
+  def dns_lookup (host: Host, domain: Domain) = Future { dnsProvider.address(host, domain) }
 
-    def ip_lookup = Future { ipProvider.address() }
-    def dns_lookup (host: Host, domain: Domain) = Future { dnsProvider.address(host, domain) }
+  val (host, domain) = hostAndDomain()
 
-    val (host, domain) = hostAndDomain()
+  val daemon = if ( args.length == 1 ) { "-d".equals(args(0)) } else { false }
 
-    val daemon = if ( args.length == 1 ) { "-d".equals(args(0)) } else { false }
+  do {
+    val externalIp = ip_lookup
+    val dnsIp      = dns_lookup(host, domain)
 
-    do {
-      val externalIp = ip_lookup
-      val dnsIp      = dns_lookup(host, domain)
+    val result = for {
+      host_ip <- externalIp
+      dns_ip <- dnsIp
+    } yield (host_ip, dns_ip)
 
-      val result = for {
-        host_ip <- externalIp
-        dns_ip <- dnsIp
-      } yield (host_ip, dns_ip)
+    if (!daemon) {
+      Await.result(externalIp, 10 second)
+      Await.result(dnsIp, 10 second)
+    } else {
+      TimeUnit.MINUTES.sleep(1L)
+    }
 
-      if (!daemon) {
-        Await.result(externalIp, 10 second)
-        Await.result(dnsIp, 10 second)
-      } else {
-        TimeUnit.MINUTES.sleep(1L)
-      }
-
-      result onComplete {
-        case Success(value) =>
-          value match {
-            case (Some(externalIp: InetAddress), Some(dnsIp: InetAddress)) =>
-              if (!externalIp.equals(dnsIp)) {
-                logger.info(s"updating DNS with $externalIp")
-                ddnsProvider.update(host, domain, externalIp)
-              }
-            case (None, Some(_)) => logger.severe("unable to fetch external IP!")
-            case (Some(_), None) => logger.severe("unable to fetch host record")
-            case (None, None) => logger.severe("unable to fetch external IP and host record")
-          }
-        case Failure(exception) => logger.severe(s"unable to process: ${exception.getMessage}")
-      }
-    } while (daemon)
-  }
+    result onComplete {
+      case Success(value) =>
+        value match {
+          case (Some(externalIp: InetAddress), Some(dnsIp: InetAddress)) =>
+            if (!externalIp.equals(dnsIp)) {
+              logger.info(s"updating DNS with $externalIp")
+              ddnsProvider.update(host, domain, externalIp)
+            }
+          case (None, Some(_)) => logger.severe("unable to fetch external IP!")
+          case (Some(_), None) => logger.severe("unable to fetch host record")
+          case (None, None) => logger.severe("unable to fetch external IP and host record")
+        }
+      case Failure(exception) => logger.severe(s"unable to process: ${exception.getMessage}")
+    }
+  } while (daemon)
 }
