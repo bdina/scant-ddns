5be5af783910183c5e96dd420f46aae4dd03eb26
Bryan Dina <bryansdina@gmail.com>
2020-11-23 15:56:05 -0500

    make core pool size (scheduled executor) 1 (from 2)

--- src/main/scala/app/Scant.scala
+++ src/main/scala/app/Scant.scala
@@ -67,7 +67,7 @@ object Scant extends App with ScantLogging with SystemManagement {
   if (!daemon) {
     Await.result(execute, 10.seconds)
   } else {
-    val exec = concurrent.ScheduledExecutionContext(corePoolSize=2)
+    val exec = concurrent.ScheduledExecutionContext(corePoolSize=1)
 
     val duration = 1.minutes
     val delay = 0.seconds
