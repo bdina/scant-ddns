4601150894470b9668350d998048f27abcb0252d
Bryan Dina <bryansdina@gmail.com>
2021-02-27 11:15:18 -0500

    test upnp address api

--- src/test/scala/protocol/UPnPExternalIPProviderTest.scala
+++ src/test/scala/protocol/UPnPExternalIPProviderTest.scala
@@ -1,27 +1,17 @@
 package protocol
 
-import collection.mutable.Stack
+import java.net.InetAddress
 
 import org.junit.runner.RunWith
 import org.scalatest._
+import org.scalatest.OptionValues._
 import flatspec._
 import matchers._
 import org.scalatestplus.junit.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
-class ExampleSpec extends AnyFlatSpec with should.Matchers {
-  "A Stack" should "pop values in last-in-first-out order" in {
-    val stack = new Stack[Int]
-    stack.push(1)
-    stack.push(2)
-    stack.pop() should be (2)
-    stack.pop() should be (1)
-  }
-
-  it should "throw NoSuchElementException if an empty stack is popped" in {
-    val emptyStack = new Stack[Int]
-    a [NoSuchElementException] should be thrownBy {
-      emptyStack.pop()
-    } 
+class UPnPExternalIPProviderSpec extends AnyFlatSpec with should.Matchers {
+  "A UPnPExternalIPProvider" should "fetch an IP from a UPnP server" in {
+    UPnPExternalIPProvider().address() shouldBe defined
   }
 }
