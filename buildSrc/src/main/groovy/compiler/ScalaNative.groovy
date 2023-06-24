a6ec6c0f6e62092f812f3058cb03d6cd5d3768f4
bdina <bryansdina@gmail.com>
2023-06-24 09:20:03 -0400

    add scala native plugin (requires scala-cli)

---
+++ buildSrc/src/main/groovy/compiler/ScalaNative.groovy
@@ -0,0 +1,69 @@
+package compiler
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.GradleException
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.InputDirectory
+import org.gradle.api.tasks.TaskAction
+
+class ScalaCli extends DefaultTask {
+    enum Option {
+        ASYNC('--Xasync')
+      , PACKAGE('--power package')
+      String arg
+      private Option(String s) { this.arg = s }
+    }
+
+    static final List<String> EXECUTABLE = [ 'scala-cli' ]
+
+    @Input
+    List<Option> parameters = [ Option.ASYNC ]
+
+    @Input
+    Integer minHeap = 1
+    @Input
+    Integer maxHeap = 32
+
+    @TaskAction
+    void runCommand() {
+        project.buildDir.mkdirs()
+
+        logger.lifecycle "Loaded project ${project} (scala ${project.ext.scala_ver})"
+        def conf = project.getConfigurations()
+        def deps = conf.getByName('compileClasspath').getAllDependencies()
+        def dStr =
+          deps
+            .collect { it ->
+              "--dependency ${it.group}:${it.name}:${it.version}"
+            }
+            .join(' ')
+        def sourceSet = project.getSourceSets().getByName('main').getAllSource()
+        def sources =
+          sourceSet
+            .findAll { file ->
+              file.path.endsWith('.scala')
+            }
+            .join(' ')
+
+        def command = "scala-cli --power package ${sources} --scala ${project.ext.scala_ver} -Xasync ${dStr} -o ${project.name} --java-opt -Xmx${maxHeap}m --java-opt -Xms${minHeap}m"
+        logger.lifecycle "Executing scala-cli command -> ${command}"
+
+        def process = command.execute(null, project.buildDir)
+        process.consumeProcessOutput(System.out, System.err)
+        process.waitFor()
+
+        if (process.exitValue() != 0) {
+            logger.error "Unable to execute scala-cli: '${process.exitValue}'"
+            throw new GradleException()
+        }
+    }
+}
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class ScalaCliPlugin implements Plugin<Project> {
+    void apply(Project project) {
+        project.task('scalaCli', type: ScalaCli)
+    }
+}
